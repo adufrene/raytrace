a7adf629b6cf077e7e8f3c1b93ef3b65adfe1ca4
Andy DuFrene <andy.dufrene94@gmail.com>
2015-03-18 22:59:07 -0700

    appears to be parallelized

--- main.go
+++ main.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"fmt"
-	//	"github.com/go-gl/mathgl/mgl64"
 	"image"
-	"image/jpeg"
+	//	"image/jpeg"
+	"image/png"
 	"math"
 	"os"
+	"runtime"
+	"strconv"
 	"strings"
+	"sync"
 )
 
 var (
 	/* For chromebook, save to downloads so file can be viewed from chromeos */
 	fileDir = os.Getenv("HOME") + "/Downloads/"
-	ext     = ".jpg"
+	//	ext     = ".jpg"
+	ext = ".png"
 
 	imgWidth  = 800
 	imgHeight = 600
@@ -28,8 +32,14 @@ var (
 
 	bkgndColor = black // fColor{R: 0.2706, G: 0.3137, B: 0.3294, A: 1.0}
 
-	MAX_DEPTH = 7
+	MAX_DEPTH  = 7
+	numThreads int
 )
+
+type goArgs struct {
+	ray  Ray
+	x, y int
+}
 
 func main() {
 	args := os.Args[1:]
@@ -50,7 +60,34 @@ func main() {
 		return
 	}
 
+	maxProcsString := os.Getenv("GOMAXPROCS")
+	if maxProcsString == "" {
+		numThreads = runtime.NumCPU()
+	} else {
+		numThreads64, err := strconv.ParseInt(maxProcsString, 10, 32)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		numThreads = int(numThreads64)
+	}
+	runtime.GOMAXPROCS(int(numThreads))
+	fmt.Println("Using", numThreads, "thread(s)")
+
+	argsChan := make(chan goArgs, 4096)
+	wg := sync.WaitGroup{}
+
 	img := image.NewRGBA(image.Rectangle{image.ZP, image.Point{imgWidth, imgHeight}})
+	for i := 0; i < numThreads; i++ {
+		wg.Add(1)
+		go func() {
+			for arg := range argsChan {
+				_, color := castRay(arg.ray, 0)
+				img.Set(arg.x, arg.y, color)
+			}
+			wg.Done()
+		}()
+	}
 
 	xTrans := eye.right.Scale(2 / float64(imgWidth))
 	yTrans := eye.up.Scale(2 / float64(imgHeight))
@@ -60,17 +97,17 @@ func main() {
 	imgPlane := eye.lookAt.Sub(eye.location).Normalize().Scale(2)
 
 	currX := xStart
-	for x := 0; x < imgWidth; x++ {
+	for x := imgWidth - 1; x >= 0; x-- {
 		currY := yStart
 		for y := imgHeight - 1; y >= 0; y-- {
 			view := eye.location.Translate(currX.Sub(eye.location)).
 				Translate(currY.Sub(eye.location)).Translate(imgPlane)
-			_, color := castRay(CreateRay(eye.location, view), 0)
-			img.Set(x, y, color)
+			argsChan <- goArgs{CreateRay(eye.location, view), x, y}
 			currY = currY.Translate(yTrans)
 		}
 		currX = currX.Translate(xTrans)
 	}
+	close(argsChan)
 
 	splitString := strings.Split(filename, "/")
 	name := splitString[len(splitString)-1]
@@ -88,7 +125,9 @@ func main() {
 		file.Close()
 	}()
 
-	err = jpeg.Encode(file, img, nil)
+	wg.Wait()
+	//	err = jpeg.Encode(file, img, nil)
+	err = png.Encode(file, img)
 	if err != nil {
 		panic(err)
 	}
